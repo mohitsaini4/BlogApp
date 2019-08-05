9ef50e36bfe1ef1390ee4d72bc2944690a076ae5
Mohit <mohitaprilsaini@gmail.com>
2019-08-05 19:27:58 -0400

    cdn of semantic UI added

--- app.js
+++ app.js
@@ -17,12 +17,6 @@ var blogSchema = new mongoose.Schema({
 });
 var Blog = mongoose.model("blogs", blogSchema);
 
-Blog.create({
-	title: "Dog",
-	image: "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-	body: "this is a dog blog"
-});
-
 //Routes
 app.get("/blogs",function(req, res){
 	Blog.find({},function(err, blog){
