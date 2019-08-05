0d4b8f79b32cb74781df4355ee8bdb9b34998948
Mohit <mohitaprilsaini@gmail.com>
2019-08-05 16:35:30 -0400

    index route created and the corresponding index.ejs created

--- app.js
+++ app.js
@@ -8,3 +8,35 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 mongoose.connect('mongodb://localhost:27017/BlogApp', {useNewUrlParser: true});
+
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created:{type: Date, default:Date.now}
+});
+var Blog = mongoose.model("blogs", blogSchema);
+
+Blog.create({
+	title: "Dog",
+	image: "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+	body: "this is a dog blog"
+});
+
+//Routes
+app.get("/blogs",function(req, res){
+	Blog.find({},function(err, blog){
+		if(err){
+			res.send("error");
+		} else{
+			res.render("index", {blogs: blog});
+		}
+	});
+	
+});
+
+
+
+app.listen(8000, function(){
+	console.log("Server Started");
+});
